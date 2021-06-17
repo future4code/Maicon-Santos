be9dcd9e1e09ad4d505bfc7be3b52035e1a789a0
maiconailton <maiconailton@hotmail.com>
2021-06-17 16:28:30 -0300

    funcoes

---
+++ semana1/operadores/index.js
@@ -0,0 +1,57 @@
+//----------------Exercícios de interpretação de código------------
+
+// ----------------------> Exercicio 1 <---------------------------
+
+/*console.log (" a.",resultado) = A, false
+console.log (" b.",resultado) = B, false
+console.log (" c.",resultado) = C, true*/
+
+// ----------------------> Exercicio 2 <---------------------------
+
+/*nesse caso vai juntar os números inves de somar , faltou converter para "Number"*/
+
+// ----------------------> Exercicio 3 <---------------------------
+
+/*let primeiroNumero = Number(prompt("Digite um numero!"))
+let segundoNumero = Number(prompt("Digite outro numero!"))
+const soma = primeiroNumero + segundoNumero
+console.log(soma)*/
+
+//------------------Exercícios de escrita de código----------------
+
+// ----------------------> Exercicio 1 <---------------------------
+
+
+/*const idade = Number(prompt("Digite a sua idade "))
+const idadeDoamigo = Number(prompt("Digite a idade do seu melhor amigo ou amiga"))
+const idades = (idade >= idadeDoamigo)
+console.log ("Sua idade é maior do que a do seu melhor amigo?", idades)
+const diferenca = (idade - idadeDoamigo)
+console.log ("Diferença de idade", diferenca)*/
+
+
+// ----------------------> Exercicio 2 <---------------------------
+
+/*const numero = Number(prompt("Insira um número par "))
+const resto = numero % 2
+console.log ("O resto da divisão por 2 é ", resto )
+// É exigibido o resto do número dividido*/
+
+// ----------------------> Exercicio 3 <---------------------------
+
+/*const ano = Number(prompt("Digite a sua idade"))
+const meses = ano * 12
+console.log ("Sua idade em meses:" ,meses)
+const dias = ano * 365
+console.log ("Sua idade em dias:", dias)
+const horas = dias * 24
+console.log ("Sua idade em horas:",horas)*/
+
+// ----------------------> Exercicio 4 <---------------------------
+
+/*let a = Number(prompt("Digite o primeiro número"))
+let b = Number(prompt("Digite o segundo número"))
+console.log ("O primeiro numero é maior que segundo?", a > b)  
+console.log ("O primeiro numero é igual ao segundo?", a==b)
+console.log ("O primeiro numero é divisível pelo segundo?",a % b ==a)
+console.log ("O segundo numero é divisível pelo primeiro?",b % a == b)*/
